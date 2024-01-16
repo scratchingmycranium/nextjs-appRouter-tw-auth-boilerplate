b2e6a66654e88bd1f625acd52a15149eb22e4be5
scratchingmycranium <simchacoleman@gmail.com>
2024-01-16 13:07:43 -0500

    Add dashboard link and create login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import { ssrWrapper } from "@/lib/api/ssrWrapper"
 import UserAPI from "@/lib/api/userAPI"
+import { RouteKeys, routeManager } from "@/lib/routeManager"
+import Link from "next/link"
 
 export default async function Home() {
-  const userAPI = ssrWrapper(UserAPI)
   
-  try {
-    const users = await userAPI.getUser()
-    console.log(users)
-  } catch (error) {
-    console.log(error)
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link
+        href={'/dashboard'}
+      >
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Dashboard</button> 
+      </Link>
     </main>
   )
 }
